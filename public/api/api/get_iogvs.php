e8ceefa49f25a9b850352322808217db806ab5c4
Maksim Kiselev (Home) <magnumru@yandex.ru>
2020-12-01 12:22:40 +0300

    fix

--- public/api/api/get_iogvs.php
+++ public/api/api/get_iogvs.php
@@ -1,6 +1,6 @@
 <?php
 
-$res = pg_query($postgres, "SELECT DISTINCT iogv FROM events ORDER BY iogvs");
+$res = pg_query($postgres, "SELECT DISTINCT iogv FROM events ORDER BY iogv");
 
 while ($row = pg_fetch_assoc($res)) {
   $result[] = $row['iogv'];
