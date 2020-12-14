8d4a6be0bcddfe9d5f389885b189ad37df804d0d
Maksim Kiselev (Home) <magnumru@yandex.ru>
2020-12-14 17:20:40 +0300

    fix

--- src/utils/request2.js
+++ src/utils/request2.js
@@ -5,7 +5,7 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: 'http://194.67.109.37:81/api',
+  baseURL: 'http://194.67.109.37:81',
   // baseURL: 'http://192.168.2.190',
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 30000 // request timeout
